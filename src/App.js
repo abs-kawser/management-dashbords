a59fb2d20c0a18f6bac2ca13ec0294f54dcb7a59
Abs kawser <abskawser77000@gmail.com>
2023-02-27 00:05:41 +0600

    Nav & SideNav donet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Sidebar from './Components/Sidebar/sidebar';
+import Navbar from './Components/Navbar/navbar';
 
 const App = () => {
   return (
     <div>
-          <h1> runging</h1>
+          {/* <h1> runging</h1> */}
           
        <Sidebar/>
+       <Navbar/>
     </div>
   );
 };
